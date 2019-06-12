b0ceca1c9d675283f53e2b828e90612901fd5912
Johann Borges <johann.borges@southsystem.com.br>
2019-06-12 11:05:42 -0300

    Updates

---
+++ precache-manifest.dc9d2f87e61ad40da7060d342bd99c55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070b920985d2c24cda095ce07dc68f42",
+    "url": "/fm-atec-tracker/index.html"
+  },
+  {
+    "revision": "87b7f61a7948b18e3e5d",
+    "url": "/fm-atec-tracker/static/css/main.28c453e4.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/fm-atec-tracker/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "87b7f61a7948b18e3e5d",
+    "url": "/fm-atec-tracker/static/js/main.3e8c2148.chunk.js"
+  },
+  {
+    "revision": "59f20b64554ba5057758",
+    "url": "/fm-atec-tracker/static/js/runtime~main.3e9b3ed4.js"
+  }
+]);
